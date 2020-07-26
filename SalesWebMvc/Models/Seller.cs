90af5af6da6bf85fb11233fdd65f8fa33d061a4a
Laureano-dev <lcftdeveloper@gmail.com>
2020-07-25 21:27:56 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,16 +9,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength (60,MinimumLength = 3,ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [EmailAddress (ErrorMessage ="Enter a valid {0}")]
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType (DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.00, 50000.00,ErrorMessage ="{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
